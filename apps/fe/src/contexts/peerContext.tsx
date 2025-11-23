a4618edbd4c3cec2dd4fd8d48b60aa91b6ab47d2
Krishna Saxena <rio17105@gmail.com>
2025-11-23 17:47:49 +0530

    Turn servers from metered added

--- apps/fe/src/contexts/peerContext.tsx
+++ apps/fe/src/contexts/peerContext.tsx
@@ -1,18 +1,18 @@
+const API_KEY = process.env.API_KEY;
+
+const response = await fetch(
+  `https://pixelverse.metered.live/api/v1/turn/credentials?apiKey=${API_KEY}`
+);
+
+const iceServers = await response.json();
+
 class PeerService {
   peer: RTCPeerConnection | null = null;
 
   constructor() {
     if (!this.peer) {
       this.peer = new RTCPeerConnection({
-        iceServers: [
-          { urls: "stun:stun.l.google.com:19302" },
-          { urls: "stun:global.stun.twilio.com:3478" },
-          {
-            urls: "relay1.expressturn.com:3480",
-            username: "000000002079099262",
-            credential: "mKSsQhe7U3r1DQZQU9kJ1Mv1Xko=",
-          },
-        ],
+        iceServers: iceServers,
       });
     }
   }
